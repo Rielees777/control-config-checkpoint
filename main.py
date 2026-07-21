bae63af2194982e5e58f6b02e592d96db4d2f1d9
krotkoiv <krotkoiv@sovcombank.ru>
2026-07-21 14:42:33 +0300

    change scheduler add cert

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ CHECK_NAME = "allowed-client-any-host"
 SEARCH_STRING = "add allowed-client host any-host"
 
 # Настройки расписания (время в UTC)
-SCHEDULE_TIME = "11:45"  # Изменить на нужное время в формате HH:MM
+SCHEDULE_TIME = "11:55"  # Изменить на нужное время в формате HH:MM
 
 
 @dataclass
